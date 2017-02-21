474403f349344bbdc136bfbe5dbf4a1782a0c161
Kirill Garbar <iterium@gmail.com>
2017-02-21 11:45:54 +0000

    [ ] extended sso

--- EveOnlineSSO.php
+++ EveOnlineSSO.php
@@ -1,6 +1,7 @@
 <?php
 namespace stiks\eveonline_sso;
 
+use Yii;
 use yii\authclient\OAuth2;
 
 class EveOnlineSSO extends OAuth2 {
@@ -11,20 +12,53 @@ class EveOnlineSSO extends OAuth2 {
     public $tokenUrl   = 'https://login.eveonline.com/oauth/token';
     public $apiBaseUrl = 'https://login.eveonline.com/oauth';
 
-    public function initUserAttributes() {
+    /**
+     * Composes user authorization URL.
+     * @param array $params additional auth GET params.
+     * @return string authorization URL.
+     */
+    public function buildAuthUrl (array $params = []) {
+        $defaultParams = [
+            'client_id' => $this->clientId,
+            'response_type' => 'code',
+            'redirect_uri' => $this->getReturnUrl(),
+            'xoauth_displayname' => Yii::$app->name,
+        ];
+
+        if (!empty ($this->scope)) {
+            $defaultParams['scope'] = implode (' ', $this->scope);
+        }
+
+        if ($this->validateAuthState) {
+            $authState = $this->generateAuthState ();
+            $this->setState('authState', $authState);
+            $defaultParams['state'] = $authState;
+        }
+
+        return $this->composeUrl ($this->authUrl, array_merge ($defaultParams, $params));
+    }
+
+    protected function defaultViewOptions () {
+        return [
+            'popupWidth' => 800,
+            'popupHeight' => 800,
+        ];
+    }
+
+    public function initUserAttributes () {
         return $this->api('verify', 'GET');
     }
 
-    public function applyAccessTokenToRequest($request, $accessToken) {
-        $request->getHeaders()->add('Authorization', 'Bearer '.$accessToken->getToken());
+    public function applyAccessTokenToRequest ($request, $accessToken) {
+        $request->getHeaders()->add('Authorization', 'Bearer '.$accessToken->getToken ());
     }
 
-    protected function defaultName() {
+    protected function defaultName () {
         return 'eveonline-sso';
     }
 
-    protected function defaultTitle() {
-        return 'EveOnline SSO';
+    protected function defaultTitle () {
+        return 'EveOnline';
     }
 }
 ?>
